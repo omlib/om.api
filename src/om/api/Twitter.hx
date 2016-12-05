cf66338029476f293e965ac10eecbe64852c8590
tong <tong@disktree.net>
2016-12-05 06:32:11 +0100

    Om R0

--- src/om/api/Twitter.hx
+++ src/om/api/Twitter.hx
@@ -8,9 +8,7 @@ class Twitter {
     public static inline var TWEET_PATH = 'intent/tweet';
 
     /**
-        Usage:
-
-        window.open( Twitter.createTweetURL( 'My text to tweet', 'http://disktree.net' ), '', 'width=618,height=382' );
+        `window.open( Twitter.createTweetURL( 'My text', 'http://disktree.net' ), '', 'width=618,height=382' );`
     */
     public static function createTweetURL( ?text : String, ?url : String, ?via : String ) : String {
         var str = '$URL/$TWEET_PATH?';
