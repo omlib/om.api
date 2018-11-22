f68fc598cd4a4a860deb3e0fec0207aa8c19fbb0
tong <tong@disktree.net>
2018-11-22 13:38:50 +0100

    ...

--- src/om/api/youtube/YouTube.hx
+++ src/om/api/youtube/YouTube.hx
@@ -5,13 +5,13 @@ import js.Browser.document;
 /**
     Utility to initialize the youtube api.
 
-    https://developers.google.com/youtube/iframe_api_reference
+    @see: https://developers.google.com/youtube/iframe_api_reference
 */
 class YouTube {
 
     @:keep
     @:expose
-    static function __onReady() callback();
+    static function __onYoutubeReady__() callback();
 
     static var callback : Void->Void;
 
@@ -19,15 +19,19 @@ class YouTube {
 
         om.api.youtube.YouTube.callback = callback;
 
-        var firstScriptElement = document.getElementsByTagName( 'script' )[0];
+        //TODO html head only
+        var scriptElements = document.getElementsByTagName( 'script' );
+        //TODO if( scriptElements.length == 0 )
+
+        var firstScriptElement = scriptElements[0];
 
         var scriptElement = document.createScriptElement();
         scriptElement.src = "https://www.youtube.com/iframe_api";
         firstScriptElement.parentNode.insertBefore( scriptElement, firstScriptElement );
 
-        var cbElement = document.createScriptElement();
-        cbElement.textContent = 'function onYouTubeIframeAPIReady(){om.api.youtube.YouTube.__onReady();}';
-        firstScriptElement.parentNode.insertBefore( cbElement, firstScriptElement );
+        var cbe = document.createScriptElement();
+        cbe.textContent = 'function onYouTubeIframeAPIReady(){om.api.youtube.YouTube.__onYoutubeReady__();}';
+        firstScriptElement.parentNode.insertBefore( cbe, firstScriptElement );
     }
 
 }
